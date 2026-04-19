2b79c065237c924be8476fb323d9cece6201c9b0
318215 <318215@studenti.unimore.it>
2026-04-19 18:59:49 +0200

    very basic version. Front end only

---
+++ WebApp/src/config/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='pages/dashboard.html'), name='dashboard'),
+    
+    # Nutrizione
+    path('nutrizione/piani/', TemplateView.as_view(template_name='pages/nutrizione/piani_list.html'), name='nutrizione_piani'),
+    path('nutrizione/anamnesi/', TemplateView.as_view(template_name='pages/nutrizione/anamnesi_create.html'), name='nutrizione_anamnesi'),
+    path('nutrizione/integratori/', TemplateView.as_view(template_name='pages/nutrizione/integratori_list.html'), name='nutrizione_integratori'),
+    
+    # Allenamenti
+    path('allenamenti/', TemplateView.as_view(template_name='pages/allenamenti/list.html'), name='allenamenti_list'),
+    path('allenamenti/crea/', TemplateView.as_view(template_name='pages/allenamenti/create.html'), name='allenamenti_create'),
+    path('allenamenti/dettaglio/', TemplateView.as_view(template_name='pages/allenamenti/detail.html'), name='allenamenti_detail'),
+]
