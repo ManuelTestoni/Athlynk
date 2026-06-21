bafdf20de72eaf33a59fb9036bd51ace053260b4
318215 <318215@studenti.unimore.it>
2026-06-21 10:16:33 +0200

    modularizing railway start

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+cd WebApp/src
+
+if [ "$RAILWAY_SERVICE_NAME" = "Athlynk" ]; then
+  python manage.py collectstatic --noinput
+  python manage.py migrate --noinput
+  python manage.py createcachetable
+  exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 4 --timeout 60
+elif [ "$RAILWAY_SERVICE_NAME" = "Analytics_Cron" ]; then
+  python manage.py run_daily_analytics
+else
+  echo "ERROR: unknown RAILWAY_SERVICE_NAME='$RAILWAY_SERVICE_NAME'" >&2
+  exit 1
+fi
